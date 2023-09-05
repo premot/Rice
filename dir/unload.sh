4302594be452e58d97db9eaa4c263fe47314f55a
premot <premote@proton.me>
2023-09-05 11:45:08 +0000

    update

---
+++ dir/unload.sh
@@ -0,0 +1,3 @@
+cp .* ~                                                                                        16:40:22
+cp hypr ~/.config/ -r                                                                          16:40:28
+cp alacritty ~/.config/ -r  
