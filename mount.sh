129c9fb9ef1833eede6fde65b2f95b0d3e14faeb
premot <premote@proton.me>
2023-09-24 16:25:43 +0300

    update

---
+++ mount.sh
@@ -0,0 +1,8 @@
+mount --types proc /proc proc
+mount --rbind /sys sys
+mount --make-rslave sys
+mount --rbind /dev dev
+mount --make-rslave dev
+mount --bind /run run
+mount --make-slave run
+chroot . /bin/bash
